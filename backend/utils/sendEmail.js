ef678e06c8e7b184c0b995803c9f35097728a21a
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-07-08 16:42:58 +0500

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,21 +1,26 @@
 const nodemailer = require('nodemailer');
+
 const frontendURL = process.env.FRONTEND_URL?.trim().replace(/\/+$/, '');
+
 exports.sendVerificationEmail = async (email, token) => {
-  console.log('Sending email')
+  console.log('Sending email');
+
   const url = `${frontendURL}/verify?token=${token}`;
 
   const transporter = nodemailer.createTransport({
-    service: 'Gmail', // or your SMTP provider
+    host: 'smtp.zoho.com',
+    port: 465,
+    secure: true, // true for port 465
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER, // e.g. 'support@notepaddle.com'
+      pass: process.env.EMAIL_PASS, // your Zoho email password
     },
   });
 
   await transporter.sendMail({
-    from: '"Notepadle" <no-reply@notepadle.com>',
+    from: `"Notepadle" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: 'Verify your email',
-    html: `<p>Click the link to verify:</p><a href="${url}">${url}</a>`,
+    html: `<p>Click the link below to verify your email:</p><a href="${url}">${url}</a>`,
   });
 };
