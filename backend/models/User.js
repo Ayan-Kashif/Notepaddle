7bf29fe2a8118957060bca6be738c82cf7187bbb
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-07-09 16:48:59 +0500

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -69,14 +69,14 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  // 💡 Skip hashing if password is already hashed
+  if (this.password.startsWith('$2')) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
+
 // Auto-add default category
 userSchema.pre('save', function(next) {
   if (this.isNew && this.categories.length === 0) {
