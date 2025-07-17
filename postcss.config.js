5451710132ff0c1a1f69d7b4b319c4b8013da023
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-07-17 10:52:02 +0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 };
