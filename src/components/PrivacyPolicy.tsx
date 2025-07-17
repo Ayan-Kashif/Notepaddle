239e904db390f3df71301acfcb8d15dbb75e1fae
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-07-18 00:27:48 +0500

    Update PrivacyPolicy.tsx

--- src/components/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import { X, Shield, Eye, Lock, Database, Users, Mail, Calendar } from 'lucide-react';
 import '../i18n';
 import { useTranslation } from 'react-i18next';
