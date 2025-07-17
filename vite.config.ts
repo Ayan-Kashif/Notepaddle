426b69c043c77143a823a120b0a38c76e931bdef
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-07-17 14:22:29 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,12 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    visualizer({ open: true })
+  ],
   build: {
     minify: 'esbuild',
     target: 'esnext',
