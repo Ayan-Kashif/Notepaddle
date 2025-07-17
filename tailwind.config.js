28be11d5007da57c428e40e13d66463569cc0098
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-07-17 10:49:40 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+//   darkMode: 'class',
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+  darkMode: 'class', // ✅ Prefer 'class' to avoid unused styles
+}
